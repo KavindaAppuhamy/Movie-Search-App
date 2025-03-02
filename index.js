529715d5aa8dffe0536588f70a8617688d7ec8b1
Kavinda Apuuhamy <145139167+KavindaAppuhamy@users.noreply.github.com>
2025-03-02 22:16:13 +0530

    Added Search Box Suggestions

--- index.js
+++ index.js
@@ -29,3 +29,37 @@ function search(){
 
     }
 } 
+
+function showSuggestions() {
+    const input = document.getElementById('movieTag').value;
+    const suggestionsBox = document.getElementById('suggestions');
+    suggestionsBox.innerHTML = '';
+    if (input.length < 2) return;
+
+    let apiKey = "ec60c24";
+    let url = "https://www.omdbapi.com/?apikey=" + apiKey + "&s=" + input;
+
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url);
+    httpRequest.responseType = "json";
+    httpRequest.send();
+
+    httpRequest.onload = function() {
+        const response = httpRequest.response;
+        if (response.Response === "True") {
+            response.Search.forEach(movie => {
+                const li = document.createElement('li');
+                li.textContent = movie.Title;
+                li.onclick = () => {
+                    document.getElementById('movieTag').value = movie.Title;
+                    suggestionsBox.innerHTML = '';
+                    search(); // Optionally trigger search immediately
+                };
+                suggestionsBox.appendChild(li);
+            });
+        }
+    };
+}
+
+// Attach event listener to the input field
+document.getElementById('movieTag').addEventListener('input', showSuggestions);
